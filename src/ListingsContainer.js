1524ae5d46c76b44b807a4550f04c9ab5026a7d1
Robert Ayers <rsheldon3ayers@gmail.com>
2020-10-12 21:15:13 -0400

    First commit

---
+++ src/ListingsContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ListingsContainer() {
+    return(
+        <div className="listings-container">
+            
+        </div>
+    );
+}
+
+export default ListingsContainer;
